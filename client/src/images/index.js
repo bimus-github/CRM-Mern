1910426eb89d3423a052105b50d7f6bb99c432a8
Madaminjon Abdullajonov <musulmon20010716@gmail.com>
2022-07-05 17:27:23 +0500

    rasmlar va ularni exporti

---
+++ client/src/images/index.js
@@ -0,0 +1,33 @@
+import chatImg from "./chat.jpg";
+import statisticsImg from "./statistics.jpg";
+import scheduleImg from "./schedule.jpg";
+import calendarImg from "./calendar.jpg";
+import paymentImg from "./payment.jpg";
+import historyImg from "./history.jpg";
+
+export const dataCard = [
+  {
+    name: "Payment",
+    img: paymentImg,
+  },
+  {
+    name: "Chedule",
+    img: scheduleImg,
+  },
+  {
+    name: "History",
+    img: historyImg,
+  },
+  {
+    name: "Notes",
+    img: chatImg,
+  },
+  {
+    name: "Statistics",
+    img: statisticsImg,
+  },
+  {
+    name: "Calendar",
+    img: calendarImg,
+  },
+];
