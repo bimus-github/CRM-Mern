2ed8367a63c3f92e231a9350e50351131a3101ff
Madaminjon Abdullajonov <musulmon20010716@gmail.com>
2022-07-05 17:23:10 +0500

    sozlamalar

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import router from "./routes/auth.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+app.use("/auths", router);
+
+const CONNECTION_URL =
+  "mongodb+srv://madaminjon:madaminjonMernCrm@cluster0.xmkf6nz.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((e) => console.log(e));
